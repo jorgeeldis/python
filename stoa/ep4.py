e35eee16494e26db9f3341d2d15f358e20f54aa1
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-17 00:22:09 -0500

    Add files via upload

---
+++ stoa/ep4.py
@@ -0,0 +1,40 @@
+#Numbers, Castings and Strings
+import random
+
+#int
+#float
+#complex
+
+x = 1000
+y = 2e3
+s = 1+5j
+
+"""
+print(type(x))
+
+print(type(y))
+
+print(type(s))
+
+print(y)
+print(s)
+"""
+
+"""
+a = float(x)
+print(a)
+
+b = int(y)
+print(b)
+
+c = complex(x)
+print(c)
+
+print(random.randrange(1,100))
+"""
+#Insert the correct syntax to convert x into a floating number
+
+StoaVariable = 4
+print(StoaVariable)
+StoaVariable = float(StoaVariable)
+print(StoaVariable)
