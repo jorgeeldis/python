97c6785b6c22b8cb2201ad811deaa06574acb104
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-18 20:59:43 -0500

    Add files via upload

---
+++ stoa/ep5.py
@@ -0,0 +1,23 @@
+#Strings
+
+
+#print('Hi')
+#print("Stoa")
+
+Name = "Stoa"
+print(Name)
+
+Sentence = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(Sentence)
+
+Company = "Stoa Community"
+print(Company[0])
+
+for x in "banana":
+    print(x)
+
+a = "aksdbsbdicno"
+print(len(a))
